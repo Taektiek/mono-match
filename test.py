b1198c2e0353d6da0dbe5aa84e5b7429a81f9793
TaekeRoukema <62061406+TaekeRoukema@users.noreply.github.com>
2021-04-30 15:06:49 +0200

    Moved around files

--- test.py
+++
@@ -1,9 +0,0 @@
-symbols = ['a', 'b', 'c', 'd']
-
-cards = [[1, 2, 3, 4], [4, 3, 2, 1], [1, 4, 2, 3], [3, 2, 1, 4]]
-
-emoji_cards = []
-for i, card in enumerate(cards):
-    emoji_cards.append([symbols[j-1] for j in card])
-
-print(emoji_cards)
